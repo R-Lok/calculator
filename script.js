05fb1e35a01d742b9caf8a238fa014c4f77dcca7
Ryan Lok <rlok.pc@gmail.com>
2022-05-15 21:12:06 +0800

    Make number buttons functional

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let display = document.querySelector('.display');
 let clearBtn = document.querySelector('.clear');
+let oneBtn = document.querySelector('.one');
+let twoBtn = document.querySelector('.two');
+let threeBtn = document.querySelector('.three');
+let fourBtn = document.querySelector('.four');
+let fiveBtn = document.querySelector('.five');
+let sixBtn = document.querySelector('.six');
+let sevenBtn = document.querySelector('.seven');
+let eightBtn = document.querySelector('.eight');
+let nineBtn = document.querySelector('.nine');
+let zeroBtn = document.querySelector('.zero');
+let addBtn = document.querySelector('.add');
+let subtractBtn = document.querySelector('.subtract');
+let multiplyBtn = document.querySelector('.multiply');
+let divideBtn = document.querySelector('.divide');
+let digitButtons = [oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, zeroBtn];
 
 function add(numOne, numTwo) {
     return numOne + numTwo;
@@ -25,8 +40,10 @@ function clearDisplay() {
     display.textContent = "";
 }
 
-clearBtn.addEventListener('click', clearDisplay);
-
-function insertOne() {
-
+function addNumber(e) {
+    display.textContent += e.target.textContent.trim();
 }
+
+
+clearBtn.addEventListener('click', clearDisplay);
+digitButtons.forEach(button => button.addEventListener('click', addNumber));
